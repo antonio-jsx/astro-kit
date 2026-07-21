2268cd04ef9ccad5605a4d56ebcd3528430eb0cf
antonio-jsx <antoniodev.1993@gmail.com>
2026-07-21 00:59:07 -0400

    contenido md, mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -14,4 +16,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx({ optimize: true })],
 });
