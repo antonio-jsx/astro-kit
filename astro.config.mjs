f6222cfd4a05ebace63ed0c68b4653f708b83bd7
antonio-jsx <antoniodev.1993@gmail.com>
2026-07-22 20:05:15 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
