ffb67cebe0adeabe6c332c2fa4a8463c7bb82788
antonio-jsx <antoniodev.1993@gmail.com>
2026-08-02 22:23:28 -0400

    paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { auth } from './lib/auth';
 import { defineMiddleware } from 'astro:middleware';
+import { auth } from '@/lib/auth';
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const { pathname } = context.url;
